dbc1fad8348b2cb3616c9b2a1b7eabd3010785ad
CaCaBlocker <Treeboy2021@outlook.com>
2022-07-07 23:00:01 +0900

    init/10

---
+++ 10.getting_started_with_fuzzing/main.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+	With fuzzing, random data is run against your test in an attempt to find vulnerabilities or crash-causing inputs.
+	Some examples of vulnerabilities that can be found by fuzzing are SQL injection, buffer overflow, denial of service and cross-site scripting attacks.
+*/
+
+import (
+	"fmt"
+	"unicode/utf8"
+	"errors"
+)
+
+func Reverse(s string) (string, error) {
+    if !utf8.ValidString(s) {
+        return s, errors.New("input is not valid UTF-8")
+    }
+    r := []rune(s)
+    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+        r[i], r[j] = r[j], r[i]
+    }
+    return string(r), nil
+}
+
+func main() {
+    input := "The quick brown fox jumped over the lazy dog"
+    rev, revErr := Reverse(input)
+    doubleRev, doubleRevErr := Reverse(rev)
+    fmt.Printf("original: %q\n", input)
+    fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+    fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
